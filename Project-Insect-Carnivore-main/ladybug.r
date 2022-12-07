ac01d54fc3e9973c50dadfd811f3b520c9f0bf9b
lukeacowan <lukecowan20@augustana.edu>
2022-12-06 20:44:38 -0600

    Cleaning the second ladybug data set

--- Project-Insect-Carnivore-main/ladybug.r
+++ Project-Insect-Carnivore-main/ladybug.r
@@ -8,7 +8,89 @@ rm(list = ls())
 setwd("~/Data 331/Final project/final_project/Project-Insect-Carnivore-main")
 
 #reading the ladybug data in
+ladybug_condensed <- read_excel("data/Ladybug Data.xlsx", .name_repair = "universal")
 ladybugs <- read.csv("data/Scan Ladybug Data.csv")
+
+found_ladybug <- ladybug_condensed %>%
+  dplyr::filter(Species != 'UNKNOWN') %>% 
+  dplyr::rename(Location = plot)
+
+location_found <- found_ladybug %>%
+  select(Species, Location)
+
+location_found$Location <- gsub("LP-AG-1", "Agriculture", location_found$Location)  
+location_found$Location <- gsub("LP-AG-2", "Agriculture", location_found$Location) 
+location_found$Location <- gsub("LP-AG-3", "Agriculture", location_found$Location) 
+location_found$Location <- gsub("LP-AG-4", "Agriculture", location_found$Location)  
+location_found$Location <- gsub("LP-AG-5", "Agriculture", location_found$Location)  
+location_found$Location <- gsub("LP-GM-1", "Mowed Grass", location_found$Location)  
+location_found$Location <- gsub("LP-GM-2", "Mowed Grass", location_found$Location) 
+location_found$Location <- gsub("LP-GM-3", "Mowed Grass", location_found$Location)  
+location_found$Location <- gsub("LP-GM-4", "Mowed Grass", location_found$Location)  
+location_found$Location <- gsub("LP-GM-5", "Mowed Grass", location_found$Location)
+location_found$Location <- gsub("LP-GU-1", "Unmowed Grass", location_found$Location)
+location_found$Location <- gsub("LP-GU-2", "Unmowed Grass", location_found$Location)
+location_found$Location <- gsub("LP-GU-3", "Unmowed Grass", location_found$Location)
+location_found$Location <- gsub("LP-GU-4", "Unmowed Grass", location_found$Location)
+location_found$Location <- gsub("LP-GU-5", "Unmowed Grass", location_found$Location)
+location_found$Location <- gsub("LP-IC-1", "Industrial", location_found$Location)
+location_found$Location <- gsub("LP-IC-2", "Industrial", location_found$Location)
+location_found$Location <- gsub("LP-IC-3", "Industrial", location_found$Location)
+location_found$Location <- gsub("LP-IC-4", "Industrial", location_found$Location)
+location_found$Location <- gsub("LP-IC-5", "Industrial", location_found$Location)
+location_found$Location <- gsub("LP-PR-1", "Prairie", location_found$Location)
+location_found$Location <- gsub("LP-PR-2", "Prairie", location_found$Location)
+location_found$Location <- gsub("LP-PR-3", "Prairie", location_found$Location)
+location_found$Location <- gsub("LP-PR-4", "Prairie", location_found$Location)
+location_found$Location <- gsub("LP-PR-5", "Prairie", location_found$Location)
+location_found$Location <- gsub("Lp-PR-5", "Prairie", location_found$Location)
+location_found$Location <- gsub("LP-GA-1", "Garden", location_found$Location)
+location_found$Location <- gsub("LP-GA-2", "Garden", location_found$Location)
+location_found$Location <- gsub("LP-GA-3", "Garden", location_found$Location)
+location_found$Location <- gsub("LP-GA-4", "Garden", location_found$Location)
+location_found$Location <- gsub("LP-GA-5", "Garden", location_found$Location)
+location_found$Location <- gsub("LP-GF-1", "Forested", location_found$Location)
+location_found$Location <- gsub("LP-GF-2", "Forested", location_found$Location)
+location_found$Location <- gsub("LP-GF-3", "Forested", location_found$Location)
+location_found$Location <- gsub("LP-GF-4", "Forested", location_found$Location)
+location_found$Location <- gsub("LP-GF-5", "Forested", location_found$Location)
+
+location_found$Species <- gsub("Propylea quatuordecimpuncata", "Propylea quatuordecimpunctata", location_found$Species)
+location_found$Species <- gsub("hippodamia parenthesis", "Hippodamia parenthesis", location_found$Species)
+location_found$Species <- gsub("hippodamia convergens", "Hippodamia convergens", location_found$Species)
+location_found$Species <- gsub("Hippodamia convergence", "Hippodamia convergens", location_found$Species)
+location_found$Species <- gsub("Hippodamia covergence", "Hippodamia convergens", location_found$Species)
+location_found$Species <- gsub("Harmonia axyrisis", "Harmonia axyridis", location_found$Species)
+location_found$Species <- gsub("harmonia axyrids", "Harmonia axyridis", location_found$Species)
+location_found$Species <- gsub("harmonia axyridis", "Harmonia axyridis", location_found$Species)
+location_found$Species <- gsub("Harminia axyridis", "Harmonia axyridis", location_found$Species)
+location_found$Species <- gsub("Cycloneda Munda", "Cycloneda munda", location_found$Species)
+location_found$Species <- gsub("cycloneda munda", "Cycloneda munda", location_found$Species)
+location_found$Species <- gsub("coleomegilla maculata", "Coleomegilla maculata", location_found$Species)
+location_found$Species <- gsub("Colemegilla maculata", "Coleomegilla maculata", location_found$Species)
+location_found$Species <- gsub("Coccinella Septempunctata", "Coccinella septemunctata", location_found$Species)
+location_found$Species <- gsub("coccinella septempunctata", "Coccinella septemunctata", location_found$Species)
+location_found$Species <- gsub("Coccinella semtempuncata", "Coccinella septemunctata", location_found$Species)
+location_found$Species <- gsub("Coccinella septempunctata", "Coccinella septemunctata", location_found$Species)
+
+
+
+
+
+
+
+
+ggplot(data = location_found, aes(x = Species, y = Location)) +
+  geom_col(position = position_dodge()) +
+  coord_flip() + scale_y_continuous(name="Number Found") +
+  #scale_x_discrete(name="Ladybug Found") 
+
+
+
+
+
+
+
 
 #copy of initial dataframe to be used for consistent formatting of state names
 ladybugs_states <- ladybugs
