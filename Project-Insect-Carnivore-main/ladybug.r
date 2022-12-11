ec1bf0bd6be1f8833602bf7d1cfa8212773176d0
lukeacowan <lukecowan20@augustana.edu>
2022-12-10 18:15:43 -0600

    Removed 2030 filler data so line graph only included real data

--- Project-Insect-Carnivore-main/ladybug.r
+++ Project-Insect-Carnivore-main/ladybug.r
@@ -168,11 +168,11 @@ ladybug_by_decade <- ladybug_decade %>%
   mutate(percentage = ladybugsFound / sum(ladybugsFound))
 
 #creating a data frame to prevent discontinuity in the line graph below due to decades where certain ladybugs were not collected
-filler_data = data.frame(decade=c("1960s","1960s", "1960s", "1970s", "1970s", "1980s", "1990s", "2000s", "2000s", "2000s", "2010s", "2010s", "2010s", "2010s", "2020s", "2030s", "2030s", "2030s", "2030s", "2030s", "2030s"), scientificName=c("Chilocorus stigma", "Coccinella septempunctata", "Harmonia axyridis", "Coccinella septempunctata", "Harmonia axyridis", "Harmonia axyridis", "Harmonia axyridis", "Chilocorus stigma", "Hippodamia convergens", "Harmonia axyridis", "Cycloneda sanguinea", "Chilocorus stigma", "Hippodamia convergens", "Harmonia axyridis", "Cycloneda sanguinea", "Cycloneda sanguinea", "Chilocorus stigma", "Coccinella septempunctata", "Coleomegilla maculata", "Hippodamia convergens", "Harmonia axyridis"), ladybugsFound=c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0), percentage=c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
+filler_data = data.frame(decade=c("1960s","1960s", "1960s", "1970s", "1970s", "1980s", "1990s", "2000s", "2000s", "2000s", "2010s", "2010s", "2010s", "2010s", "2020s"), scientificName=c("Chilocorus stigma", "Coccinella septempunctata", "Harmonia axyridis", "Coccinella septempunctata", "Harmonia axyridis", "Harmonia axyridis", "Harmonia axyridis", "Chilocorus stigma", "Hippodamia convergens", "Harmonia axyridis", "Cycloneda sanguinea", "Chilocorus stigma", "Hippodamia convergens", "Harmonia axyridis", "Cycloneda sanguinea"), ladybugsFound=c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0), percentage=c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))
 ladybug_by_decade <- rbind(ladybug_by_decade, filler_data)
 
 #    Talk about how sample sizes in decades leading up to 2020 do not offer a complete picture due to the lack of ladybugs recorded
-#    Totals: 1960s - 12; 1970s - 27; 1980s - 35; 1990s - 22; 2000s - 3; 2010s - 3
+#    Totals: 1960s - 12; 1970s - 27; 1980s - 35; 1990s - 22; 2000s - 3; 2010s - 3; 2020s - 294
 #    Mention how the most common types throughout these decades were selected -- did not include all distinct ladybugs
 
 #ladybug proportion by decade visualization
